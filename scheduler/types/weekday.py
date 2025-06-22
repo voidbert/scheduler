4c2ee39e0a4669af419fffc650e0c85be09c0a89
Voidbert <humbertogilgomes@protonmail.com>
2025-06-22 15:02:30 +0100

    fix: mismatch between __str__ and __repr__

--- scheduler/types/weekday.py
+++ scheduler/types/weekday.py
@@ -1,12 +1,9 @@
 import enum
 
 @enum.unique
-class Weekday(enum.Enum):
-    MONDAY = 1
-    TUESDAY = 2
-    WEDNESDAY = 3
-    THURSDAY = 4
-    FRIDAY = 5
-
-    def __repr__(self) -> str:
-        return str(self)[len('Weekday') + 1:].capitalize()
+class Weekday(enum.StrEnum):
+    MONDAY = 'Monday'
+    TUESDAY = 'Tuesday'
+    WEDNESDAY = 'Wednesday'
+    THURSDAY = 'Thursday'
+    FRIDAY = 'Friday'
--- tests/types/test_weekday.py
+++ tests/types/test_weekday.py
@@ -1,7 +1,7 @@
 from scheduler.types.weekday import Weekday
 
 def test_repr():
-    assert repr(Weekday.WEDNESDAY) == 'Wednesday'
+    assert repr(Weekday.WEDNESDAY) == '<Weekday.WEDNESDAY: \'Wednesday\'>'
 
 def test_str():
-    assert str(Weekday.MONDAY) == 'Weekday.MONDAY'
+    assert str(Weekday.MONDAY) == 'Monday'
