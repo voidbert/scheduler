bc85dc4b2244b938dc46a4c0833c712c5c739230
Voidbert <humbertogilgomes@protonmail.com>
2025-06-22 15:18:21 +0100

    fix: missing type annotations

--- tests/types/test_weekday.py
+++ tests/types/test_weekday.py
@@ -1,7 +1,7 @@
 from scheduler.types.weekday import Weekday
 
-def test_repr():
+def test_repr() -> None:
     assert repr(Weekday.WEDNESDAY) == '<Weekday.WEDNESDAY: \'Wednesday\'>'
 
-def test_str():
+def test_str() -> None:
     assert str(Weekday.MONDAY) == 'Monday'
