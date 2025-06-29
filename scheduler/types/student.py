6841707d47014e3932152d26935697b4fc9ce274
voidbert <humbertogilgomes@protonmail.com>
2025-06-29 02:30:22 +0100

    feat: Student documentation

--- scheduler/types/student.py
+++ scheduler/types/student.py
@@ -5,9 +5,22 @@ import typing
 from .course import Course
 
 class StudentError(Exception):
+    '''Type of exception thrown by :class:`Student`.'''
     pass
 
 class Student:
+    '''
+    A student enrolled in the university. It is characterized by its mechanographic number and the
+    courses they are enrolled in.
+
+    :param number:  Mechanographic number of the student, that identifies it.
+    :param courses: List of courses the student is enrolled in. No courses will be copied. When
+                    ``None`` (default), no courses will be added to the student.
+
+    :raises StudentError: ``courses`` has more than one course with the same
+                           :attr:`~.course.Course.id`.
+    '''
+
     def __init__(self, number: str, courses: None | list[Course] = None) -> None:
         self.__number = number
         self.__courses: dict[str, Course] = {}
@@ -17,6 +30,20 @@ class Student:
                 self.add_course(course)
 
     def add_course(self, course: Course) -> None:
+        '''
+        Adds a course to the list of courses the student is enrolled in.
+
+        :param course: Course to enroll the student in. It will not be copied.
+
+        :raises StudentError: The student is already enrolled in a course with the same
+                              :attr:`~.course.Course.id`.
+
+        >>> student = Student('A10400')
+        >>> student.add_course(Course('Software Labs II'))
+        >>> student.courses
+        {'Software Labes II': Course(name='Software Labs II', shifts={})}
+        '''
+
         if course.id in self.__courses:
             raise StudentError('Tried to add a course to a student more than once')
 
@@ -24,10 +51,27 @@ class Student:
 
     @property
     def number(self) -> str:
+        '''
+        Mechanographic number of the student, that identifies it.
+
+        >>> Student('A104000').number
+        'A104000'
+        '''
+
         return self.__number
 
     @property
     def courses(self) -> dict[str, Course]:
+        '''
+        Association between course identifiers (:attr:`~.course.Course.id`) and the courses the
+        student is enrolled in.
+
+        **A copy of the dictionary will be returned**, but the references to the courses will not.
+
+        >>> Student('A104000', [Course('Software Labs II')]).courses
+        {'Software Labes II': Course(name='Software Labs II', shifts={})}
+        '''
+
         return copy.copy(self.__courses)
 
     def __eq__(self, other: typing.Any) -> bool:
